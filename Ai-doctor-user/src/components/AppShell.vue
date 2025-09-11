300cb8e5a08ce1a2ceb201d0ec820550e65bbb0c
Edison <edison_z.f._zhong/cen/foxconn@foxconn.com>
2025-09-11 14:45:53 +0800

    实现欢迎页布局样式编写

--- Ai-doctor-user/src/components/AppShell.vue
+++ Ai-doctor-user/src/components/AppShell.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="chat-layout">
         <ChatHistory />
-        <ChatWindow />
+        <!-- <ChatWindow /> -->
+        <ChatWelcome />
     </div>
 </template>
 
 <script setup lang="ts">
 import ChatHistory from './ChatHistory.vue';
 import ChatWindow from './ChatWindow.vue';
+import ChatWelcome from './ChatWelcome.vue';
 </script>
 
 <style scoped lang="less">
---
+++ Ai-doctor-user/src/components/ChatWelcome.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="chat-window">
+    <div class="chat-message">
+        <img src="../assets/zhanshi.png" alt="">
+        <span class="title">我是Ai医生助手，很高兴见到你!</span>
+        <span class="subtitle">无论你想了解什么疾病的症状、治疗方法，还是关系日常养生，我都能为你提供科学的解答和建议</span>
+    </div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="less">
+.chat-window {
+    width: 100%;
+    margin-left: 230px;
+    display: flex;
+    .chat-message {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        max-width: 1000px;
+        margin: 0 auto;
+        padding-bottom: 90px;
+        img {
+            width: 280px;
+        }
+        .title {
+            font-size: 25px;
+            padding-top: 15px;
+        }
+        .subtitle {
+            padding-top: 15px;
+        }
+    }
+}
+</style>
