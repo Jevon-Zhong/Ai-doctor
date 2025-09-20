2fe3ee069bb0128cf640e04060336d82fc67fe90
Edison <edison_z.f._zhong/cen/foxconn@foxconn.com>
2025-09-20 17:30:00 +0800

    完成知识库文件列表获取以及对话框文件上传和删除

--- ai-doctor-server/src/filemanagement/filemanagement.controller.ts
+++ ai-doctor-server/src/filemanagement/filemanagement.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Delete, Post, Req, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Post, Req, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Filemanagement } from './filemanegement.schema';
@@ -81,6 +81,64 @@ export class FilemanagementController {
         const { docId } = body
         const userId = req.user.token
         return await this.filemanagementService.deleteFileKb(userId, docId)
+    }
+
+    //获取知识库文件列表
+    @Get('kbfilelist')
+    @UseGuards(AuthGuard)
+    async kbFileList(
+        @Req() req: { user: { token: string } }
+    ) {
+        const userId = req.user.token
+        const res = await this.FilemanagementModel.aggregate([
+            { $match: { userId, uploadType: 'UB' } },
+            {
+                $project: {
+                    docId: '$_id',
+                    fileName: 1,
+                    fileType: 1,
+                    fileSize: 1,
+                    _id: 0
+                }
+            }
+        ])
+        return {
+            result: res
+        }
+    }
+
+    //对话框文件上传
+    @Post('uploaddialog')
+    @UseGuards(AuthGuard)
+    @UseInterceptors(uploadFileInterceptor)
+    async uploadDialog(
+        @Req() req: { user: { token: string } },
+        @UploadedFiles() files: { file: Express.Multer.File[] }) {
+        const userId = req.user.token
+        const documentIds: Types.ObjectId[] = []
+        //处理上传到文档(for遍历处理多文件，推荐使用队列处理)
+        for (const file of files.file) {
+            //1.读取文档
+            const { rawText } = await this.filemanagementService.readFile(file, 'UB')
+            //2.上传数据库
+            const docID = await this.filemanagementService.uploadFile(file, userId, rawText, 'UB')
+            documentIds.push(docID)
+        }
+        return {
+            result: documentIds
+        }
+    }
+
+    //对话框删除文件
+    @Delete('deletefile')
+    @UseGuards(AuthGuard)
+    async deleteFile(
+        @Req() req: { user: { token: string } },
+        @Body() body: DeletefileDto
+    ) {
+        const { docId } = body
+        const userId = req.user.token
+        return await this.filemanagementService.deleteFile(userId, docId)
     }
 }
 
